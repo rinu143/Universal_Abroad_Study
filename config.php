a2010cf3993be486c1b5711efc6851bdd8c5f12f
Rinu <rinuathul402@gmail.com>
2025-10-25 23:34:37 +0530

    fix: Removed config.php from gitignore

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// SMTP credentials
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_USERNAME', 'universalabroadstudies@gmail.com');
+define('SMTP_PASSWORD', 'chqo lupp flwg imnx');
+define('SMTP_PORT', 587);
+// The email address you want to send from
+define('SMTP_FROM_EMAIL', 'universalabroadstudies@gmail.com');
+// The name you want to send from
+define('SMTP_FROM_NAME', 'Universal Abroad Studies');
+?>
